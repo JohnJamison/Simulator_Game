7d205b73439b1dfc0231e429f6c6871eebee458d
John Jamison <www.johnjamison@gmail.com>
2026-04-10 18:55:04 -0700

    Delete Desktop/signuptest directory

--- Desktop/signuptest/lib/models/announcement.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Announcement {
-  final IconData icon;
-  final String title;
-  final String? subtitle;
-  final Color tint;
-  const Announcement(this.icon, this.title, {this.subtitle, this.tint = const Color(0xFFB80F0A)});
-}
